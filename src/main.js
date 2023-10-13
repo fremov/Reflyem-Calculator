75283c3de97b9ffd226322e144bbaf55bf22d88d
fremov <59342062+fremov@users.noreply.github.com>
2023-10-13 10:58:18 +0300

    added another stone choose

--- src/main.js
+++ src/main.js
@@ -63,6 +63,31 @@ radioButtons.forEach(function (radioButton) {
                         stones.Speech.value = Number(stones.Speech.value) - 5;
                         stones.Alchemy.value = Number(stones.Alchemy.value) - 5;
                         break;
+                    case 'stoneAnother':
+                        // здесь нужно написать вычитание 5 из полей для случая "rogue"
+                        stones.Evasion.value = Number(stones.Evasion.value) - 5;
+                        stones.Stealth.value = Number(stones.Stealth.value) - 5;
+                        stones.Breaking.value = Number(stones.Breaking.value) - 5;
+                        stones.Pickpocketing.value = Number(stones.Pickpocketing.value) - 5;
+                        stones.Speech.value = Number(stones.Speech.value) - 5;
+                        stones.Alchemy.value = Number(stones.Alchemy.value) - 5;
+
+                        // здесь нужно написать вычитание 5 из полей для случая "mage"
+                        stones.Illusion.value = Number(stones.Illusion.value) - 5;
+                        stones.Destruction.value = Number(stones.Destruction.value) - 5;
+                        stones.Witchcraft.value = Number(stones.Witchcraft.value) - 5;
+                        stones.Recovery.value = Number(stones.Recovery.value) - 5;
+                        stones.Change.value = Number(stones.Change.value) - 5;
+                        stones.Enchantment.value = Number(stones.Enchantment.value) - 5;
+
+                        stones.Blacksmith.value = Number(stones.Blacksmith.value) - 5;
+                        stones.OneHandedWeapon.value = Number(stones.OneHandedWeapon.value) - 5;
+                        stones.HeavyArmor.value = Number(stones.HeavyArmor.value) - 5;
+                        stones.TwoHandedWeapon.value = Number(stones.TwoHandedWeapon.value) - 5;
+                        stones.Shooting.value = Number(stones.Shooting.value) - 5;
+                        stones.Blocking.value = Number(stones.Blocking.value) - 5;
+
+                        break;
                 }
             }
             // добавляем 5 к полю выбранной кнопки
@@ -101,6 +126,39 @@ radioButtons.forEach(function (radioButton) {
                     stones.Pickpocketing.value = Number(stones.Pickpocketing.value) + 5;
                     stones.Speech.value = Number(stones.Speech.value) + 5;
                     stones.Alchemy.value = Number(stones.Alchemy.value) + 5;
+                    break;
+                case 'stoneAnother':
+                    // здесь нужно написать добавление 5 к полям для случая "rogue"
+                    chosenStone.innerText = 'Вор';
+                    chosenStone.classList.remove('text-danger');
+                    chosenStone.classList.add('text-success');
+                    stones.Evasion.value = Number(stones.Evasion.value) + 5;
+                    stones.Stealth.value = Number(stones.Stealth.value) + 5;
+                    stones.Breaking.value = Number(stones.Breaking.value) + 5;
+                    stones.Pickpocketing.value = Number(stones.Pickpocketing.value) + 5;
+                    stones.Speech.value = Number(stones.Speech.value) + 5;
+                    stones.Alchemy.value = Number(stones.Alchemy.value) + 5;
+
+                    // здесь нужно написать добавление 5 к полям для случая "mage"
+                    chosenStone.innerText = 'Маг';
+                    chosenStone.classList.remove('text-danger');
+                    chosenStone.classList.add('text-success');
+                    stones.Illusion.value = Number(stones.Illusion.value) + 5;
+                    stones.Destruction.value = Number(stones.Destruction.value) + 5;
+                    stones.Witchcraft.value = Number(stones.Witchcraft.value) + 5;
+                    stones.Recovery.value = Number(stones.Recovery.value) + 5;
+                    stones.Change.value = Number(stones.Change.value) + 5;
+                    stones.Enchantment.value = Number(stones.Enchantment.value) + 5;
+
+                    chosenStone.innerText = 'Воин';
+                    chosenStone.classList.remove('text-danger');
+                    chosenStone.classList.add('text-success');
+                    stones.Blacksmith.value = Number(stones.Blacksmith.value) + 5;
+                    stones.OneHandedWeapon.value = Number(stones.OneHandedWeapon.value) + 5;
+                    stones.HeavyArmor.value = Number(stones.HeavyArmor.value) + 5;
+                    stones.TwoHandedWeapon.value = Number(stones.TwoHandedWeapon.value) + 5;
+                    stones.Shooting.value = Number(stones.Shooting.value) + 5;
+                    stones.Blocking.value = Number(stones.Blocking.value) + 5;
                     break;
             }
             lastSelectedStone = radioButton; // сохраняем текущую кнопку в lastSelected
