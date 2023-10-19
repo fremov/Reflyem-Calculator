c2f4965c3ac26e7b5fc50a77d2705033593fe27f
fremov <59342062+fremov@users.noreply.github.com>
2023-10-19 22:30:18 +0300

    added dynamic difficulty

--- src/js/lp_calc_funcs.js
+++ src/js/lp_calc_funcs.js
@@ -19,12 +19,36 @@ export let chosenLevel = document.getElementById('chosenLevel');
 const radio1 = document.getElementById('tenLP');
 const radio2 = document.getElementById('fifteenLp');
 const radio3 = document.getElementById('twentyLP');
+const radio4 = document.getElementById('dynamicLP');
+
+const content = document.getElementById('toggleHidden');
 
 // Обработчик события изменения состояния radio button
 function handleRadioChange() {
     const lpSetting = getLPSetting()
-
-    needLvlForUp = Math.ceil(Number(allLPTable.innerHTML) / lpSetting);
+    if (radio4.checked) {
+        content.style.display = "block";
+        console.log('radio4')
+        let totalLP = Number(allLPTable.innerHTML)
+        let nowLevel = Number(document.getElementById('nowLevel').value)
+        while (totalLP > 0) {
+            let levelLP = 15
+            if (nowLevel >= 20 && nowLevel < 40) {
+                levelLP = 10
+                console.log(nowLevel)
+            }
+            else if (nowLevel >= 40) {
+                levelLP = 5
+                console.log(nowLevel)
+            }
+            totalLP -= levelLP
+            nowLevel += 1
+        }
+        needLvlForUp = nowLevel
+    }
+    else {
+        needLvlForUp = Math.ceil(Number(allLPTable.innerHTML) / lpSetting);
+    }
     if (needLvlForUp > 0) {
         if (chosenStone.innerText === 'Ученик') {
             chosenLevel.innerText = String(`${lpSetting - 1}LP`);
@@ -46,12 +70,16 @@ function getLPSetting() {
     const base = chosenStone.innerText === "Ученик" ? 1 : 0;
     switch (true) {
         case radio1.checked:
+            content.style.display = "none";
             return base + 10;
         case radio2.checked:
+            content.style.display = "none";
             return base + 15;
         case radio3.checked:
+            content.style.display = "none";
             return base + 20;
         default:
+            content.style.display = "none";
             return base + 15;
     }
 }
@@ -60,6 +88,7 @@ function getLPSetting() {
 radio1.addEventListener('change', handleRadioChange);
 radio2.addEventListener('change', handleRadioChange);
 radio3.addEventListener('change', handleRadioChange);
+radio4.addEventListener('change', handleRadioChange);
 
 // присваивается значение в зависимоти от уровня сложности и потом на него делить
 export function calculate_needed_lvl() {
